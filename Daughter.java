b023669e60861ceaf1b9bdd1cb98da3a1b9aa1a8
prajaktajadhav <prajakta171195@gmail.com>
2022-07-14 11:41:26 +0530

    hi this my file

---
+++ Daughter.java
@@ -0,0 +1,11 @@
+package Inheritance;
+
+public class Daughter extends mother {
+	
+	public void naughty() {
+	System.out.println("very naughty");
+	}
+	public static void game() {
+		System.out.println("addicted for game");
+	}
+}
